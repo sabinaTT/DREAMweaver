7e3edd20dde46efc2755898cb7fadaa7bdfaf1a8
sabinaTT <tairsbn@gmail.com>
2022-01-27 19:19:04 -0800

    server connect

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const methodOverride = require('method-override')
+
+
+const routes = require('./routes')
+const app = express()
+
+
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+
+
+// app.use('/', routes.dreams)
+
+
+const PORT = 3000;
+
+
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+app.listen(PORT, function() {
+    console.log(`dreams live on http://localhost:${PORT}`)
+})
