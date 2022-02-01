7960b87452b66a78f03cf4837cb24ffe2c1fa036
evankeith <evanmkeith@gmail.com>
2022-01-31 17:59:23 -0800

    Set-up base views and profile route

--- routes/dreams.js
+++ routes/dreams.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const ctrl = require('../controllers');
 
 router.get('/', ctrl.dreams.index);
+router.get('/', crtl.dreams.profile);
 
 module.exports = router
