cdd17701a0742b0ca4ee9388f71c7ca7e10c9cb4
sabinaTT <tairsbn@gmail.com>
2022-02-01 18:06:46 -0800

    create ejs page for dreamers

--- controllers/dreams.js
+++ controllers/dreams.js
@@ -33,4 +33,5 @@ module.exports = {
     index,
     profile, 
     about,
+    dreams,
 }
--- routes/dreams.js
+++ routes/dreams.js
@@ -4,6 +4,6 @@ const ctrl = require('../controllers');
 router.get('/', ctrl.dreams.index);
 router.get('/profile', ctrl.dreams.profile);
 router.get('/about', ctrl.dreams.about)
-router.get('/', ctrl.dreams.dreams)
+router.get('/dreams', ctrl.dreams.dreams)
 
 module.exports = router
