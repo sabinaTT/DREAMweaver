5ad8880c805f3cf789b8f1786d6069783a0094bc
sabinaTT <tairsbn@gmail.com>
2022-02-01 18:01:09 -0800

    add route for dreams

--- routes/dreams.js
+++ routes/dreams.js
@@ -4,5 +4,6 @@ const ctrl = require('../controllers');
 router.get('/', ctrl.dreams.index);
 router.get('/profile', ctrl.dreams.profile);
 router.get('/about', ctrl.dreams.about)
+router.get('/', ctrl.dreams.dreams)
 
 module.exports = router
