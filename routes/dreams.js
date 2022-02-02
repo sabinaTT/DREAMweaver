d63f7f81386890de31fef5f431b42e4a68e67223
sabinaTT <tairsbn@gmail.com>
2022-02-01 17:48:15 -0800

    create about page

--- routes/dreams.js
+++ routes/dreams.js
@@ -3,5 +3,6 @@ const ctrl = require('../controllers');
 
 router.get('/', ctrl.dreams.index);
 router.get('/profile', ctrl.dreams.profile);
+router.get('/about', ctrl.dreams.about)
 
 module.exports = router
